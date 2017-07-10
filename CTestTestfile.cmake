d0d81602b4a9dd7dffa57962421fc3c4b15084bd
Bruno Berisso <bruno@tryolabs.com>
2017-07-10 15:24:44 +0100

    Setup build system

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/brunoberisso/Documents/Tryolabs/pegaces-wavelib
+# Build directory: /Users/brunoberisso/Documents/Tryolabs/pegaces-wavelib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("unitTests")
+subdirs("src")
---
+++ unitTests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/brunoberisso/Documents/Tryolabs/pegaces-wavelib/unitTests
+# Build directory: /Users/brunoberisso/Documents/Tryolabs/pegaces-wavelib/unitTests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("wavelibBoostTests")
